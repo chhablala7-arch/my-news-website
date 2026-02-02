37053c5d44ddd9498e3daccfe7cf4d53d249a460
chhablala7-arch <chhablala7@gmail.com>
2026-02-02 20:55:13 +0530

    Update firebase-app.js

--- firebase-app.js
+++ firebase-app.js
@@ -1,21 +1,11 @@
-<!-- Firebase v8 SDKs -->
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js"></script>
+const firebaseConfig = {
+  apiKey: "AIzaSyD8syqOEEwNxsEUb8tIPL5BFWnBhgsGJg8",
+  authDomain: "chhablal-login.firebaseapp.com",
+  projectId: "chhablal-login",
+  storageBucket: "chhablal-login.firebasestorage.app",
+  messagingSenderId: "234736579008",
+  appId: "1:234736579008:web:04caf7443e035ad0e21825"
+};
 
-<script>
-  var firebaseConfig = {
-    apiKey: "AIzaSyD8syqOEEwNxsEUb8tIPL5BFWnBhgsGJg8",
-    authDomain: "chhablal-login.firebaseapp.com",
-    projectId: "chhablal-login",
-    storageBucket: "chhablal-login.firebasestorage.app",
-    messagingSenderId: "234736579008",
-    appId: "1:234736579008:web:04caf7443e035ad0e21825"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  var auth = firebase.auth();
-  var db = firebase.firestore();
-</script>
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
