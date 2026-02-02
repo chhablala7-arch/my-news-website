73f961246841a6b0dd104f5380e9e653bd10645e
chhablala7-arch <chhablala7@gmail.com>
2026-02-02 08:42:13 +0530

    Create firebase-app.js

---
+++ admin/firebase-app.js
@@ -0,0 +1,14 @@
+// Firebase SDK (compat is already loaded in HTML)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyQqYtGm-cc6cTB8wxG3oVun3tAfic",
+  authDomain: "chhabalal-31cea.firebaseapp.com",
+  projectId: "chhabalal-31cea",
+  storageBucket: "chhabalal-31cea.firebasestorage.app",
+  messagingSenderId: "473513653439",
+  appId: "1:473513653439:web:cdd2acdbe40fae65fdc20a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
