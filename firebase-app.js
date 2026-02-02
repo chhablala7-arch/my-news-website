d0358d7b0b2b83d240e6c53e8d937e0e475d1bd5
chhablala7-arch <chhablala7@gmail.com>
2026-02-02 16:14:22 +0530

    Update firebase-app.js

--- firebase-app.js
+++ firebase-app.js
@@ -11,4 +11,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore(); 
+const db = firebase.firestore();  // 👈 important for admin.js
