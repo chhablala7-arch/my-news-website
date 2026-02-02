8a3dfba1ff415da9936031e74bd1bcd7fe95950b
chhablala7-arch <chhablala7@gmail.com>
2026-02-03 00:27:55 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,4 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
-
-<script>
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyD8syqOEEwNxsEUb8tIPL5BFWnBhgsGJg8",
   authDomain: "chhablal-login.firebaseapp.com",
   projectId: "chhablal-login",
@@ -12,10 +7,5 @@ const firebaseConfig = {
   appId: "1:234736579008:web:04caf7443e035ad0e21825"
 };
 
-if(!firebase.apps.length){
-  firebase.initializeApp(firebaseConfig);
-}
-
-const auth = firebase.auth();
-const db = firebase.firestore();
-</script>
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
