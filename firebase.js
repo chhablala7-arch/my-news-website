271b40d50c27c456179a8cda134bf2410ee7fbd6
chhablala7-arch <chhablala7@gmail.com>
2026-02-03 10:12:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,19 @@
+// Firebase v9 modular
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyD8syqOEEwNxsEUb8tIPL5BFWnBhgsGJg8",
-  authDomain: "chhablal-login.firebaseapp.com",
-  projectId: "chhablal-login",
-  storageBucket: "chhablal-login.firebasestorage.app",
-  messagingSenderId: "234736579008",
-  appId: "1:234736579008:web:04caf7443e035ad0e21825"
+  apiKey: "AIzaSyCyQqYc5YtGm-cc6cTB8wxG3oVun3tAfic",
+  authDomain: "chhabalal-31cea.firebaseapp.com",
+  projectId: "chhabalal-31cea",
+  storageBucket: "chhabalal-31cea.firebasestorage.app",
+  messagingSenderId: "473513653439",
+  appId: "1:473513653439:web:cdd2acdbe40fae65fdc20a"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
