2d596ced6c108f8a6467c0d77e6b077d1c8a63a1
chhablala7-arch <chhablala7@gmail.com>
2026-02-03 08:45:27 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,15 @@
-var firebaseConfig = {
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyD8syqOEEwNxsEUb8tIPL5BFWnBhgsGJg8",
   authDomain: "chhablal-login.firebaseapp.com",
   projectId: "chhablal-login",
-  storageBucket: "chhablal-login.appspot.com",
+  storageBucket: "chhablal-login.firebasestorage.app",
   messagingSenderId: "234736579008",
   appId: "1:234736579008:web:04caf7443e035ad0e21825"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
